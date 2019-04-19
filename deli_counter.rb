258fc22a8da66744383b8c0490c8d8b366f06ebc
zhenggao cai <caizhengao1992@gmail.com>
2019-04-19 00:27:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
     
   else
-    katz_deli.each_with_index.map {|name,index| puts "The line is currently: #{index + 1}. #{name}"}
+    katz_deli.each_with_index.map {|name,index|"#{index + 1}. #{name}"}
   end
 end
 def take_a_number(katz_deli,name)
