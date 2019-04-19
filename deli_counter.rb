a49bd83d86ffbd349296040458ee6c94a16f6c69
zhenggao cai <caizhengao1992@gmail.com>
2019-04-19 00:29:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,12 +5,15 @@ def line(katz_deli)
     puts "The line is currently empty."
     
   else
-    katz_deli.each_with_index.map {|name,index|"#{index + 1}. #{name}"}
+    array=katz_deli.each_with_index.map {|name,index|" #{index + 1}. #{name}"}
+    string=array.join
+    puts "The line is currently:" << string
+    
   end
 end
 def take_a_number(katz_deli,name)
-  katz_deli=katz_deli << name
-  katz_deli.each_with_index.map {|name,index| "Welcome, #{name}. You are number #{index + 1} in line."}
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size}."
 end
 def now_serving(katz_deli)
   if katz_deli.size==0
